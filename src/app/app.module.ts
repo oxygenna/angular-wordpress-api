340bb2f114a0e858fa0413e77fd1abb773fbf745
John Langan <john.langan@oxygenna.com>
2018-05-29 16:52:49 +0300

    Step 3: Add Angular Flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppMaterialModule } from './app-material.module';
     BrowserModule,
     BrowserAnimationsModule
     AppMaterialModule,
+    FlexModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
